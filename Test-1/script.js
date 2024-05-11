e61848d21213448271b80b64d837cbd4af54b955
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-05-11 15:54:00 +0530

    update

---
+++ Test-1/script.js
@@ -0,0 +1,76 @@
+// Question 1:
+// Write a JavaScript for loop that iterates from 0 to 10 and prints only the even numbers.
+;
+
+// for(let i = 0; i<10; i++){
+//   if(i == 0){
+//      console.log("even");
+//   }
+//   else{
+//     console.log("odd");
+
+//   }
+// }
+
+// Question 2:
+
+// Write a JavaScript while loop that calculates the factorial of a given number.
+
+// let number = 4;
+// let i = 1;
+// let temp = 1;
+
+// while(i<number){
+//     temp = temp*i
+//     i++
+// }
+// console.log(temp);
+
+
+// Question 3:
+
+// Create a JavaScript function that takes an array of numbers as input and returns the sum of all the numbers using a for loop.
+
+// let array = [1,2,3,4,5]
+
+// function sumofarray(array){
+//     var sum = 0;
+//     for(var i = 0; i<=array.length; i++){
+//       sum +=i
+
+//     }
+//    console.log(sum);
+// }
+// sumofarray(array)
+
+
+// Question 4:
+
+// Write a JavaScript program that generates a Fibonacci sequence up to a certain number n, using a while loop.
+
+//   function fibonacci(number){
+//    var a = 0;
+//    var b = 1;
+//    console.log(a);
+//    console.log(b);
+//    var c;
+//    c= a+b;
+
+//     while (c<=number) {
+//         console.log(c);
+//         a= b;
+//         b=c;
+//         c = a+b
+//     }
+
+//   }
+//   fibonacci(8)
+
+
+// Question 5:
+
+// Create a JavaScript function that takes an array of strings as input and returns a new array with only the strings that have a length greater than 5 characters.
+
+var array =["virat","rohit","risabh","furkan"]
+
+
