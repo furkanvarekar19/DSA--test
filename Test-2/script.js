eacb372a1147253d98281efb5f7ee5cfe4a4e38d
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-05-19 12:09:06 +0530

    update

--- Test-2/script.js
+++ Test-2/script.js
@@ -1,12 +1,84 @@
 // Question 1:
 // Write a JavaScript function that takes an array of numbers as input and returns a new array with each number squared.
-function squareNumbers(numbers) {
-    return numbers.map(function(number) {
-      return number * number;
-    });
-  }
-  
-  const inputArray = [1, 2, 3, 4, 5];
-  const squaredArray = squareNumbers(inputArray);
-  console.log(squaredArray); // Output: [1, 4, 9, 16, 25]
-  
+
+// array= [1,2,3,4,5];
+// array1=[];
+// function square(array){
+// for(let i =0;i<array.length;i++){
+//   // console.log(array[i]);
+//   var square1=array[i]* array[i];
+//   array1.push(square1);
+
+// }
+// return array1;
+// }
+//   const array2 = square(array);
+//   console.log(array2);
+ 
+// Question 4:
+// Create a JavaScript function that takes an object and a key as input, and returns the value associated with that key in the object. If the key does not exist, return a default value
+
+// var info = {name:"Alice",age:25}
+// function fetchvalue(){
+//   // console.log(info.name);
+
+// }
+// fetchvalue(info);
+
+// Question 6:
+// Create a JavaScript function that takes a string as input and returns a new string with each character duplicated
+
+// function duplicatevalue(str){
+//   for(var i = 0;i<str.length;i++){
+//     var str2="";
+//     str2 += str[i] + str[i]
+//     console.log(str2);
+//   }
+// }
+// duplicatevalue("hello");
+
+// Question 9:
+// Write a JavaScript function that takes an array of numbers and returns the second smallest number in the array.
+  // array =[10,2,14,5,7];
+  // function secondsmall(array){
+  //   array.sort((a,b)=>a-b)
+  //   return array[1]
+
+  // }
+  // console.log(secondsmall(array));
+
+  // Question 2:
+// Create a JavaScript function that checks if a given string is a palindrome (reads the same forwards and backwards).
+
+// function palindrome(name){
+//   var str= "";
+//   for(var i=name.length-1;i>=0;i--){
+//     // console.log(name[i]);
+//     str+=name[i];
+//   }
+//   if(name===str){
+//     console.log("true");
+//   }else{
+//     console.log("false");
+//   }
+// }
+// palindrome("madam");
+
+// Question 8:
+// Create a JavaScript function that takes a sentence as input and returns the number of words in the sentence.
+
+// function countwords(sentence){
+//   var count = 0;
+//   for(var i=0; i<sentence.length;i++){
+//     if(sentence[i]=== ""){
+//       count++
+
+//     }
+//     count++;
+//     console.log(count);
+
+//   }
+// }
+// countwords("this is a sample sentence");
+
+
